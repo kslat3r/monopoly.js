be0029ab1ec27e34d447b8c95a203afcc51f5d96
Ed Kelly <ed@newlinelabs.com>
2013-11-29 15:48:00 +0000

    Deploy scripts complete

--- devops/mongoimport.sh
+++ devops/mongoimport.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mongorestore -h paulo.mongohq.com:10090 -u remote --password $1 -d app19861741 db/dump/monopolyjs/
+mongorestore -h paulo.mongohq.com:10090 -u remote --password $1 -d app19861741 ./db/dump/monopolyjs/
