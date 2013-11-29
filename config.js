16c4a98146b6382741f7e14c9ef4e9c8688e8a9e
Ed Kelly <ed@newlinelabs.com>
2013-11-29 15:12:09 +0000

    Adding connect async to provider

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 var db = {
-	host: '127.0.0.1',
-	name: 'monopolyjs',
-	port: 27017
+	uri: process.env.MONGOHQ_URL || 'mongodb://127.0.0.1:27017/monopolyjs'	
 };
 
 module.exports.db = db;
--- providers/provider.js
+++ providers/provider.js
@@ -3,28 +3,48 @@ var Db 			= require('mongodb').Db;
 var Server		= require('mongodb').Server;
 
 exports.Provider = function() {
-	this.db = new Db(Config.db.name, new Server(Config.db.host, Config.db.port, {safe: false}, {auto_reconnect: true}, {}));
-  	this.db.open(function() {
-
-  	});
+	this.db = null;  	
 };
 
 exports.Provider.prototype = {
+	connect: function(callback) {
+		if (this.db == null) {
+			(function(that) {
+				Db.connect(Config.db.uri, function(err, db) {
+					if (err) {
+						console.log(err);
+					}
+					else {
+						that.db = db;
+						callback();
+					}
+				});
+			})(this);
+		}
+		else {
+			callback();
+		}
+	},
+
 	list: function(collectionName, callback) {
-		this.db.collection(collectionName, function(error, collection) {
-	    	if (error) {
-	    		console.log(error);
-	    	}
-	    	else {
-	    		collection.find().toArray(function(error, docs) {
-	    			if (error) {
-	    				console.log(error);
-	    			}
-	    			else {
-        				callback(docs);
-        			}
-      			});
-	    	}
-	  	});
+		(function(that) {
+			that.connect(function() {
+				that.db.collection(collectionName, function(error, collection) {
+			    	if (error) {
+			    		console.log(error);
+			    	}
+			    	else {
+			    		collection.find().toArray(function(error, docs) {
+			    			if (error) {
+			    				console.log(error);
+			    			}
+			    			else {
+			    				callback(docs);
+			    			}
+			  			});
+			    	}
+			  	});	
+			});
+		})(this);
 	}
 }
