ab7ed4a615d67b693c5afd30290019d1172e0b30
Ed Kelly <ed@newlinelabs.com>
2013-12-07 11:13:01 +0000

    Moving private API keys to environment variables

--- config.js
+++ config.js
@@ -3,14 +3,18 @@ var db = {
 };
 
 var facebook = {
-	app_id: process.env.NODE_ENV == 'production' ? '274770006003387' : '597969390251431',
-	app_secret: process.env.NODE_ENV == 'production' ? '3498708d12cb968dde2d91a204632890' : '54a6b160db81be26c1fda2bea1638812',
+	//app_id: process.env.NODE_ENV == 'production' ? '274770006003387' : '597969390251431',
+	//app_secret: process.env.NODE_ENV == 'production' ? '3498708d12cb968dde2d91a204632890' : '54a6b160db81be26c1fda2bea1638812',
+	app_id: process.env.FACEBOOK_APP_ID,
+	app_secret: process.env.FACEBOOK_APP_SECRET,
 	callback_url: process.env.NODE_ENV == 'production' ? 'http://www.monopolyjs.com/auth/facebook/callback' : 'http://monopolyjs.nll:3000/auth/facebook/callback'
 };
 
 var twitter = {
-	consumerKey: process.env.NODE_ENV == 'production' ? 'UWVGydtdP31urWJDP0ej1g' : '9r0gFcxsQw7OakfeKXlCxA',
-	consumerSecret: process.env.NODE_ENV == 'production' ? 'X3ZpLULSLR4AEjiuNdX1jlYqa4KYkVK9sK8TVPpSQ0' : 'WQU3gUaZo5neHgmP2Fgdn152yqg2rCZiqvkg1827I',
+	//consumerKey: process.env.NODE_ENV == 'production' ? 'UWVGydtdP31urWJDP0ej1g' : '9r0gFcxsQw7OakfeKXlCxA',
+	//consumerSecret: process.env.NODE_ENV == 'production' ? 'X3ZpLULSLR4AEjiuNdX1jlYqa4KYkVK9sK8TVPpSQ0' : 'WQU3gUaZo5neHgmP2Fgdn152yqg2rCZiqvkg1827I',
+	consumerKey: process.env.TWITTER_CONSUMER_KEY,
+	consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
 	callbackURL: process.env.NODE_ENV == 'production' ? 'http://www.monopolyjs.com/auth/twitter/callback' : 'http://monopolyjs.nll:3000/auth/twitter/callback'
 };
 
--- start-dev.sh
+++ start-dev.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+export FACEBOOK_APP_ID='597969390251431'
+export FACEBOOK_APP_SECRET='54a6b160db81be26c1fda2bea1638812'
+export TWITTER_CONSUMER_KEY='9r0gFcxsQw7OakfeKXlCxA'
+export TWITTER_CONSUMER_SECRET='WQU3gUaZo5neHgmP2Fgdn152yqg2rCZiqvkg1827I'
 mongod &
 nodemon -L app.js &
 grunt &
